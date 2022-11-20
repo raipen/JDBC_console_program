e6e63962d26164d34e5549cad23a5843e680e0cd
김진현 <kimjhyun0627@gmail.com>
2022-11-20 15:22:02 +0900

    GM structure change

--- src/GameManager/GameManagerMain.java
+++ src/GameManager/GameManagerMain.java
@@ -1,31 +1,44 @@
 package GameManager;
 
-import DTO.UserDTO;
+import java.util.*;
+
+import DTO.*;
 import page.*;
 import util.*;
 
 public class GameManagerMain extends Page
 {
+	UserDAO_GM gmDAO = UserDAO_GM.getInstance();
+
 	public GameManagerMain()
 	{
 		System.out.println("관리자님 환영합니다.");
 
-		addMenu(new Menu("계정 관리")
+		addMenu(new Menu("전체 계정 조회")
 		{
 			public void execute()
 			{
-				new UserControl().start();
+				List<UserDTO> userList = gmDAO.getUserList(null);
+				System.out.println("----------전체 계정 조회 결과-----------");
+				System.out.println("ID              PW");
+				for (UserDTO U : userList)
+					System.out.println(U.getUserID() + "\t" + U.getPassword());
 			};
 		});
 
-		addMenu(new Menu("맵 관리")
+		addMenu(new Menu("전체 캐릭터 조회")
 		{
 			public void execute()
 			{
-				new MapControl().start();
+				List<CharacterDTO> characterList = gmDAO.getCharacterList(null);
+				System.out.println("----------전체 캐릭터 조회 결과----------");
+				System.out.println("캐릭터 이름"+"\tLV	EXP	스킬");
+				for (CharacterDTO U : characterList)
+					System.out.println(U.getcharacterName() + "\t\t" + U.getLv() + "\t"
+							+ U.getExp() + "\t" + U.getSkillID());
 			};
 		});
-		
+
 		addMenu(new Menu("로그아웃", true)
 		{
 			public void execute()
