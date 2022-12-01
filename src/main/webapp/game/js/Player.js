a1fc5583f62b73e18d2a1729a283cc8fef3a0d17
Jiwon <raipen@knu.ac.kr>
2022-12-02 03:26:50 +0900

    update Player.js

--- src/main/webapp/game/js/Player.js
+++ src/main/webapp/game/js/Player.js
@@ -14,7 +14,7 @@ export default class Player{
     }
 
     async init(){
-        this.character = {characterName:"테스트캐릭터",level:1,exp:0};
+        this.character = {characterName:"테스트캐릭터",level:1,exp:0,skillId:"03000015"};
         this.skill = {skillName:"dash",img:"",duration:0,cooltime:5};
         this.ability = {speed:10,life:3,cooldown:0};
         this.item = [{itemName:"테스트아이템",itemId:1,img:"",count:5}];
@@ -38,7 +38,7 @@ export default class Player{
 
     async setSkill(){
         const url = '../apis/getSkill.jsp';
-        const data = {characterId:this.characterId};
+        const data = {skillId:this.character.skillId};
         this.skill = await ajax(url,data);
     }
 
@@ -50,7 +50,7 @@ export default class Player{
 
     async setItem(){
         const url = '../apis/getItem.jsp';
-        const data = {characterId:this.characterId};
+        const data = {userId:this.userId};
         this.item = await ajax(url,data);
     }
 
