0ab319706cc7f34d69148b5eaf62b8888b861628
Jiwon <raipen@knu.ac.kr>
2022-12-01 22:55:39 +0900

    fix merge error

--- src/main/webapp/game/js/Map.js
+++ src/main/webapp/game/js/Map.js
@@ -11,30 +11,33 @@ export default class Map{
     }
 
     async init(){
-        this.mapInfo={mapName:"테스트맵",backgroundIMG:"",width:150,height:35,goalx:149,goaly:32,difficulty:1};
-        this.bases=[{x:0,y:34,width:150,height:1},{x:30,y:30,width:5,height:1},{x:32,y:31,width:1,height:4},
-                    {x:45,y:29,width:5,height:1},{x:55,y:31,width:5,height:1},{x:65,y:28,width:5,height:1},{x:130,y:27,width:5,height:1}];
-        this.hurdles=[{x:10,y:33,width:1,height:1,damage:1},{x:20,y:33,width:1,height:1,damage:1},{x:40,y:33,width:1,height:1,damage:1}];
-
-        // await this.setMapInfo();
-        // await this.setBases();
-        // await this.setHurdles();
+        //this.mapInfo={mapName:"테스트맵",backgroundIMG:"",width:150,height:35,goalx:149,goaly:32,difficulty:1};
+        //this.bases=[{x:0,y:34,width:150,height:1},{x:30,y:30,width:5,height:1},{x:32,y:31,width:1,height:4},
+        //            {x:45,y:29,width:5,height:1},{x:55,y:31,width:5,height:1},{x:65,y:28,width:5,height:1},{x:130,y:27,width:5,height:1}];
+        //this.hurdles=[{x:10,y:33,width:1,height:1,damage:1},{x:20,y:33,width:1,height:1,damage:1},{x:40,y:33,width:1,height:1,damage:1}];
+		console.log("before init");
+        await this.setMapInfo();
+		console.log(this.mapInfo);
+        await this.setBases();
+		console.log(this.bases);
+        await this.setHurdles();
+		console.log(this.hurdles);
     }
 
     async setMapInfo(){
-        const url = '../apis/getMapInfo.jps';
+        const url = '../apis/getMapInfo.jsp';
         const data = {mapNo:this.mapNo};
         this.mapInfo = await ajax(url,data);
     }
 
     async setBases(){
-        const url = '../apis/getBases.jps';
+        const url = '../apis/getBases.jsp';
         const data = {mapNo:this.mapNo};
         this.bases = await ajax(url,data);
     }
 
     async setHurdles(){
-        const url = '../apis/getHurdles.jps';
+        const url = '../apis/getHurdles.jsp';
         const data = {mapNo:this.mapNo};
         this.hurdles = await ajax(url,data);
     }
